1653c94500033b66b988ab17e0202b255edea63b
mohammedabdelnabe.23@gmail.com <mohammedabdelnabe.23@gmail.com>
2024-12-11 19:35:44 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let h = document.querySelector('.img');
+let container=document.querySelector('.container');
+function chang(img){
+    h.src=img;
+}
+function colors(color){
+    container.style.background = color;
+}
